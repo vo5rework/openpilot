73a81cc7a3f9726941e193c5b8660170c9693755
gavrackerss <72343150+vo5rework@users.noreply.github.com>
2026-01-30 14:36:03 +0000

    Update params_keys.h

--- common/params_keys.h
+++ common/params_keys.h
@@ -129,7 +129,6 @@ inline static std::unordered_map<std::string, ParamKeyAttributes> keys = {
     {"UptimeOffroad", {PERSISTENT, FLOAT, "0.0"}},
     {"UptimeOnroad", {PERSISTENT, FLOAT, "0.0"}},
     {"Version", {PERSISTENT, STRING}},
-
     {"TinklaFollowDistance", {PERSISTENT, FLOAT, "1.45"}},
     {"TinklaHandsOnLevel", {PERSISTENT, FLOAT, "2.0"}},
     {"TinklaRadarOffset", {PERSISTENT, FLOAT, "0.0"}},
